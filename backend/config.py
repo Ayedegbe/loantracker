837d94b6b430677e759c1a48cb80687a52e6f477
Daniel Ayedegbe <danielayedegbe1@gmail.com>
2025-05-26 19:20:23 +0100

    update

--- backend/config.py
+++ backend/config.py
@@ -3,7 +3,7 @@ from datetime import timedelta
 
 class Config:
     BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'database.db')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get("SECRET_KEY") or "your-secret-key"
     JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY") or "your-jwt-secret-key"
